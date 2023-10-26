b70e337b6a29f0953b36a64971aacf75235b1444
developer-jyyun <developer.jyyun@gmail.com>
2023-10-26 14:36:29 +0900

    Design: Add createGlobalStyle

--- my-sns/src/App.tsx
+++ my-sns/src/App.tsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./Pages/Home";
 import Profile from "./Pages/Profile";
 import Login from "./Pages/Login";
 import SignUp from "./Pages/SignUp";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
 
 const router = createBrowserRouter([
   {
@@ -24,10 +25,31 @@ const router = createBrowserRouter([
     element: <SignUp />,
   },
 ]);
+
+const GlobalStyles = createGlobalStyle`
+${reset};
+*{box-sizing: border-box;}
+body{
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+  "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+  sans-serif;
+  color:#fff;
+  background:#5fa578;
+}
+`;
+
 function App() {
   return (
     <>
+      <GlobalStyles />
       <RouterProvider router={router} />
+      <ul>
+        <li>dddd</li>
+        <li>dddd</li>
+        <li>dddd</li>
+        <li>dddd</li>
+        <li>dddd</li>
+      </ul>
     </>
   );
 }
