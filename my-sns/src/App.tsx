8dfe47ddf5df95e5e0e8465eca06406eae5793b0
developer-jyyun <developer.jyyun@gmail.com>
2023-10-26 14:09:37 +0900

    Feat: Create browser routing

--- my-sns/src/App.tsx
+++ my-sns/src/App.tsx
@@ -1,9 +1,33 @@
 import { useState } from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./Pages/Home";
+import Profile from "./Pages/Profile";
+import Login from "./Pages/Login";
+import SignUp from "./Pages/SignUp";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { path: "", element: <Home /> },
+      { path: "/profile", element: <Profile /> },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/signup",
+    element: <SignUp />,
+  },
+]);
 function App() {
   return (
     <>
-      <h1>🍒🍒 MY-SNS 🍒🍒</h1>
+      <RouterProvider router={router} />
     </>
   );
 }
---
+++ my-sns/src/Pages/Profile.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Profile() {
+  return (
+    <div>
+      <h1>프로필 🙋‍♀️</h1>
+    </div>
+  );
+}
