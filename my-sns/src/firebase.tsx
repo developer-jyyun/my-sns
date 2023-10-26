8d04a7307cecb47017a3375f29b56ef81bc48b9f
developer-jyyun <developer.jyyun@gmail.com>
2023-10-26 15:33:32 +0900

    Feat: Add Firebase sdk

---
+++ my-sns/src/firebase.tsx
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBmZqsQO2KfGNSMuMdQ4rZQEJdzm7yV0wM",
+  authDomain: "my-sns-550be.firebaseapp.com",
+  projectId: "my-sns-550be",
+  storageBucket: "my-sns-550be.appspot.com",
+  messagingSenderId: "962009941697",
+  appId: "1:962009941697:web:f41edf963377abf2b23640",
+};
+
+const app = initializeApp(firebaseConfig);
