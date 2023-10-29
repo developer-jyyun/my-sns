7f797644e47f47f49622e83620539e76d316e516
developer-jyyun <developer.jyyun@gmail.com>
2023-10-29 19:00:00 +0900

    Design:Modify Navigation

--- my-sns/src/components/Layout.tsx
+++ my-sns/src/components/Layout.tsx
@@ -1,81 +1,80 @@
-import React from "react";
 import { Link, Outlet, useNavigate } from "react-router-dom";
+import { auth } from "../firebase";
 import { styled } from "styled-components";
 import {
   RiLogoutCircleRLine,
   RiHomeHeartFill,
   RiUser3Fill,
 } from "react-icons/ri";
-import { auth } from "../firebase";
 
 export default function Layout() {
   const navigate = useNavigate();
-  const onLogout = async () => {
+  const onLogOut = async () => {
     const ok = confirm("로그아웃 하시겠습니까?");
     if (ok) {
       await auth.signOut();
       navigate("/signin");
     }
-    console.log("logout");
   };
   return (
-    <div>
-      <h1> layout</h1>
+    <Wrapper>
+      <Nav>
+        <Link to="/">
+          <NavItem>
+            <RiHomeHeartFill className="icon" />
+          </NavItem>
+        </Link>
+        <Link to="/profile">
+          <NavItem>
+            <RiUser3Fill className="icon" />
+          </NavItem>
+        </Link>
+        <NavItem onClick={onLogOut} className="log-out icon">
+          <RiLogoutCircleRLine />
+        </NavItem>
+      </Nav>
       <Outlet />
-      <Wrapper>
-        <Nav>
-          <NavItem>
-            <Link to="/">
-              <RiHomeHeartFill />
-            </Link>
-          </NavItem>
-          <NavItem>
-            <Link to="/profile">
-              <RiUser3Fill />
-            </Link>
-          </NavItem>
-          <NavItem className="logout">
-            <RiLogoutCircleRLine onClick={onLogout} />
-          </NavItem>
-        </Nav>
-      </Wrapper>
-    </div>
+    </Wrapper>
   );
 }
-
-const NavItem = styled.div`
-  cursor: pointer;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border: 2px solid #fff;
-  border-radius: 50px;
-  width: 50px;
-  height: 50px;
-  font-size: 20px;
-  & a {
-    color: #fff;
-  }
-  &.logout {
-    border-color: OrangeRed;
-    color: OrangeRed;
-  }
-`;
-
-const Nav = styled.nav`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  gap: 14px;
-`;
 
 const Wrapper = styled.div`
   display: grid;
   gap: 20px;
   grid-template-columns: 1fr 4fr;
-  padding: 50px 0;
+  align-items: center;
+  height: 100%;
+  padding: 50px 0px;
   width: 100%;
   max-width: 860px;
-  height: 100%;
+`;
+
+const Nav = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+`;
+
+const NavItem = styled.div`
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 2px solid white;
+  height: 50px;
+  width: 50px;
+  border-radius: 50%;
+  & .icon {
+    font-size: 26px;
+    fill: white;
+  }
+  &.log-out {
+    border-color: tomato;
+    svg {
+      font-size: 26px;
+      fill: tomato;
+    }
+  }
 `;
